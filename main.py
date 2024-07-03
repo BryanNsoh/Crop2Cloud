5e48e2c338422e3c1e63da90cfde698b2a75710a
Bryan_Nsoh <bryan.anye.5@gmail.com>
2024-07-03 13:50:38 -0500

    chore: Update configuration file paths and remove unused code

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import os
 from datetime import datetime
 from src import (
-    connect_to_datalogger, get_tables, get_data, determine_time_range,
-    update_system_time, get_schema, update_bqtable,
-    setup_database, insert_data_to_db, get_latest_timestamp,
-    send_lora_data, load_config, load_sensor_metadata, get_logger
+    connect_to_datalogger,
+    get_tables,
+    get_data,
+    determine_time_range,
+    update_system_time,
+    get_schema,
+    update_bqtable,
+    setup_database,
+    insert_data_to_db,
+    get_latest_timestamp,
+    send_lora_data,
+    load_config,
+    load_sensor_metadata,
+    get_logger,
 )
 from dotenv import load_dotenv
 
@@ -12,10 +22,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Load configuration
-config = load_config('config.yaml')
+config = load_config("config/config.yaml")
 
 # Set up custom logger
 logger = get_logger(__name__)
+
 
 def main():
     try:
@@ -23,10 +34,10 @@ def main():
         update_system_time()
 
         # Load sensor metadata
-        sensor_metadata = load_sensor_metadata(config['sensor_metadata'])
+        sensor_metadata = load_sensor_metadata(config["sensor_metadata"])
 
         # Connect to datalogger
-        datalogger = connect_to_datalogger(config['datalogger'])
+        datalogger = connect_to_datalogger(config["datalogger"])
 
         # Get table names
         table_names = get_tables(datalogger)
@@ -35,7 +46,7 @@ def main():
             return
 
         # Get latest timestamp from local database
-        latest_time = get_latest_timestamp(config['database']['name'])
+        latest_time = get_latest_timestamp(config["database"]["name"])
 
         # Determine start and stop times
         start, stop = determine_time_range(latest_time)
@@ -48,20 +59,21 @@ def main():
 
         # Update BigQuery table
         schema = get_schema(table_data)
-        update_bqtable(schema, config['cloud']['table_id'], table_data)
+        update_bqtable(schema, config["cloud"]["table_id"], table_data)
 
         # Update local SQLite database
-        setup_database(schema, config['database']['name'])
-        insert_data_to_db(table_data, config['database']['name'])
+        setup_database(schema, config["database"]["name"])
+        insert_data_to_db(table_data, config["database"]["name"])
 
         # Send data via LoRa
-        send_lora_data(table_data, config['lora'], sensor_metadata)
+        send_lora_data(table_data, config["lora"], sensor_metadata)
 
         logger.info("Data processing and upload successful!")
 
     except Exception as e:
         logger.error(f"An error occurred in the main process: {e}", exc_info=True)
         print(f"An error occurred: {e}")
+
 
 if __name__ == "__main__":
     main()
